508f3061f4483115adda58310dc466fd3acea5ed
Argenis Navas <tuemailejemplo.com>
2025-10-12 18:04:03 -0300

    subida de los primeros archivos de la pre entrega

---
+++ test/test_saucedemo.py
@@ -0,0 +1,31 @@
+import pytest
+
+
+
+
+
+@pytest.fixture
+def driver():
+    # configuracion para consultar a selenium web driver
+
+def test_login():
+    # logeo de usuario con username y password
+    # click al boton de login
+    # se redirige a la pagina de inventario
+    # verificar si el titulo correcto de la pagina
+    #
+
+def test_catalogo():
+    #logeo de usuario con username y password
+    #click en el boton de login 
+    #poder verificar el titulo pero en HTML
+    #Comprobar si existen producto en la pagina visible (len())
+    #
+
+     
+def test_carrito():
+    #logeo de usuario con username y password
+    #click en login
+    #llevarme al carrito de compras
+    #incremento de el carrito al agregar producto
+    #comprobar que el carrito aparezca el producto correctamente
