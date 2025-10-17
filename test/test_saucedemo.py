6b8364c3c16bc347c750171a1411d45923fc10de
Argenis Navas <tuemailejemplo.com>
2025-10-17 00:05:12 -0300

    Corrige función login_saucedemo

--- test/test_saucedemo.py
+++ test/test_saucedemo.py
@@ -1,31 +1,46 @@
 import pytest
+import sys
+from selenium.webdriver.common.by import By
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+
+from utils.helpers import login_saucedemo, get_driver
 
 
 
 
-
-@pytest.fixture
+@pytest.fixture(scope='session')
 def driver():
     # configuracion para consultar a selenium web driver
+    driver = get_driver()
+    yield driver
+    driver. quit()
 
-def test_login():
-    # logeo de usuario con username y password
-    # click al boton de login
-    # se redirige a la pagina de inventario
-    # verificar si el titulo correcto de la pagina
-    #
+#def test_login(driver):
 
-def test_catalogo():
-    #logeo de usuario con username y password
-    #click en el boton de login 
-    #poder verificar el titulo pero en HTML
-    #Comprobar si existen producto en la pagina visible (len())
-    #
+    #login_saucedemo(driver)
+    #assert "/inventory.html" in driver.current_url
+    #titulo = driver.find_element(By.CSS_SELECTOR, 'div.header_secondary_container .title').text
+    #assert titulo == 'Products'
 
-     
-def test_carrito():
-    #logeo de usuario con username y password
-    #click en login
-    #llevarme al carrito de compras
-    #incremento de el carrito al agregar producto
-    #comprobar que el carrito aparezca el producto correctamente
+
+
+def test_catalogo(driver):
+    login_saucedemo ( driver )
+
+    products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
+    assert len(products) > 0
+
+def test_carrito(driver):
+    login_saucedemo (driver)
+
+    products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
+    total_products = len(products)
+
+    if total_products >=2:
+        products [0]. find_element(By.TAG_NAME, 'button').click()
+        products [1]. find_element(By.TAG_NAME, 'button').click()
+
+
+    badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
+    assert int (badge) == 2
