bc06ff6b966e0c2e210dfa4e7b92b62a5b538b18
argenac12 <tuemailejemplo.com>
2025-10-22 18:40:10 -0300

    actualizacion de preentrega

--- test/test_saucedemo.py
+++ test/test_saucedemo.py
@@ -5,8 +5,11 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
 from utils.helpers import login_saucedemo, get_driver
-
-
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture(scope='session')
@@ -15,6 +18,40 @@ def driver():
     driver = get_driver()
     yield driver
     driver. quit()
+
+
+#def test_login(driver):
+    #login_saucedemo(driver)
+    
+    #title = driver.find_element(By.CLASS_NAME, 'title').text
+    #assert title == "Products"
+
+#def test_catalogo(driver):
+    #login_saucedemo(driver)
+    
+    #products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
+    #assert len(products) > 0
+
+#def test_carrito(driver):
+    #login_saucedemo(driver)
+    
+    #wait = WebDriverWait(driver, 10)
+    #products = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'inventory_item')))
+    
+    #if len(products) >= 2:
+        # Click en primer producto
+        #button1 = products[0].find_element(By.TAG_NAME, 'button')
+        #button1.click()
+        
+        # Click en segundo producto
+        #button2 = products[1].find_element(By.TAG_NAME, 'button')
+        #button2.click()
+        
+        # Esperar y verificar el badge
+        #badge_element = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'shopping_cart_badge')))
+        #badge_text = badge_element.text
+        #assert badge_text == 2
+
 
 def test_login(driver):
 
@@ -35,10 +72,12 @@ def test_carrito(driver):
     login_saucedemo (driver)
 
     products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
-    total_products = len(products)
+    
+    total_products = len(products) 
 
     if total_products >=2:
         products [0]. find_element(By.TAG_NAME, 'button').click()
+        
         products [1]. find_element(By.TAG_NAME, 'button').click()
 
 
--- utils/helpers.py
+++ utils/helpers.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import time
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
 
 URL = 'https://www.saucedemo.com/'
 USERNAME = 'standard_user'
@@ -19,7 +21,7 @@ def get_driver():
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
 
-    time.sleep(5)
+    #time.sleep(5)
 
     return driver
 
@@ -27,11 +29,18 @@ def login_saucedemo(driver):
 
     driver.get(URL)
 
+
     #ingresar credenciales
+
+    #WebDriverWait (driver, 10).until(
+        #EC.element_to_be_clickable((By.NAME,'user-name'))                    
+    #).send_keys(USERNAME)
+
     driver.find_element(By.NAME, 'user-name').send_keys(USERNAME)
     driver.find_element(By.NAME, 'password').send_keys(PASSWORD)
     driver.find_element(By.ID, 'login-button').click()
 
 
     time.sleep(7)
+    driver.implicitly_wait(5)
 
